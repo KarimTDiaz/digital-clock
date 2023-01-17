01131cfc7c9a0bec25e1de112049114bb149a8df
AlejandroJimenezMurciano <karimtilouni94@gmail.com>
2023-01-17 12:24:59 +0100

    Reloj digital

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -14,6 +14,7 @@ const daysOfWeek = ['Monday', 'Tuesday' , 'Wednesday' , 'Tuesday', 'Friday', 'Sa
 const months = ["January","February","March","April","May","June","July", "August","September","October","November","December"];
 
 const rootStyles = document.documentElement.style;
+
 let degSeconds;
 let degMinutes;
 let degHours;
@@ -21,19 +22,17 @@ let degHours;
 const timeFix = n => (n <10 ? '0' + n : n)
 
 const printHour = (date) =>{
-    
     hourElement.textContent = timeFix(date.getHours());
     minutesElement.textContent = timeFix(date.getMinutes());
     secondsElement.textContent = timeFix(date.getSeconds());
-    degSeconds = (date.getSeconds() / 60) * 360;
-    degMinutes = (date.getMinutes()/ 60) * 360 + (date.getSeconds() / 60) * 6;
-    degHours = (date.getHours() /12 * 360) + (date.getMinutes() / 60) * 30;
+    degSeconds = (date.getSeconds() * 6);
+    degMinutes = (date.getMinutes()* 6) + (date.getSeconds() / 10) ;
+    degHours = (date.getHours() * 30) + (date.getMinutes() / 2);
 console.log(degMinutes)
 
    rootStyles.setProperty('--seconds-rotate', degSeconds +'deg')
    rootStyles.setProperty('--minutes-rotate' , degMinutes + 'deg')
    rootStyles.setProperty('--hours-rotate' , degHours + 'deg')
-    
 }
 
 const printDate = (date) =>{
